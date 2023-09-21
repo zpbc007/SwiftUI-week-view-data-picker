4de91dc0bfad6923756dd6472be191978da3a49c
zhaopeng <719197352@qq.com>
2023-09-21 14:48:32 +0800

    FEAT: SwipeView work!

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "Date-Picker-Week-View",
     platforms: [
-        .iOS(.v13),
+        .iOS(.v16),
         .macOS(.v10_15),
     ],
     products: [
